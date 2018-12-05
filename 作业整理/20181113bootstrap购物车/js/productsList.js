04e9aac69600471f9b9f8a5608ce9e6f20816b1c
pickJasmin <508437281@qq.com>
2018-12-05 10:22:29 +0800

    嗯嗯

--- 作业整理/20181113bootstrap购物车/js/productsList.js
+++ 作业整理/20181113bootstrap购物车/js/productsList.js
@@ -1,24 +1,26 @@
-//创建一个变量来存储产品“数据库”,存储homeproducts.json
-var products;
+//创建一个变量来存储产品“数据库”,存储productsList.json
+var productsList;
 //使用XHR检索，将responseType设置为json，并报告错误
 //发生在XHR操作中。如果复制成功，则将产品设置为相等
 //请求。响应，然后运行initialize()函数
 
 var request = new XMLHttpRequest();
-request.open('GET', 'homeproducts.json');
+request.open('GET', 'productsList.json');
 request.responseType = 'json';
 
 request.onload = function () {
   if (request.status === 200) {
-    products = request.response;
-    console.log(products);
+    productsList = request.response;
+    console.log(productsList);
     //调用函数
     initialize();
   } else {
-    console.log('Network request for products.json failed with response ' + request.status + ': ' + request.statusText)
+    console.log('网络请求productsList:json响应失败 ' + request.status + ': ' + request.statusText)
   }
 };
 
 request.send();
 
-function initialize(){}
+function initialize(){
+  
+}
